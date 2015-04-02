19987fdc0f9cea901fb565d9d3023512958291b3
Michael Maurer <mjmaurer7@gatech.edu>
2015-04-01 20:20:06 -0400

    Got the twitter api up and running

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, redirect
+from flask import Flask, session, redirect, request
 import tweepy
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -6,12 +6,15 @@ app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
-
+consumer_key = 'lVf4ernDov9p75lJ5COIAcUiB'
+consumer_secret = 'XxLd0mZQj6GQtDoaJvbBZlDmEkMqQe1xISCWB1UK9vglMQpHEA'
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    auth = tweepy.OAuthHandler('lVf4ernDov9p75lJ5COIAcUiB', 'XxLd0mZQj6GQtDoaJvbBZlDmEkMqQe1xISCWB1UK9vglMQpHEA')
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+    api = tweepy.API(auth)
 
     try:
         redirect_url = auth.get_authorization_url()
@@ -26,8 +29,27 @@ def hello():
 @app.route('/auth')
 def twit_auth():  
 
-    return "Here"
+    verifier = request.args.get('oauth_verifier')
 
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.request_token = session['request_token']
+    del session['request_token']
+
+    try:
+        auth.get_access_token(verifier)
+    except tweepy.TweepError:
+        return 'Error! Failed to get access token.'
+
+    session['access_token'] = auth.access_token
+    session['access_token_secret'] = auth.access_token_secret
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(session['access_token'], session['access_token_secret'])
+
+    api = tweepy.API(auth)
+    api.update_status(status='tweepy + oauth!')
+
+    return "DID ITT"
 
 
 @app.errorhandler(404)
