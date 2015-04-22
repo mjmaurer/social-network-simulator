741f66004e490859adb25408d6d8868ee68a2ca1
Lauren <lwinston@gatech.edu>
2015-04-22 15:26:29 -0400

    think I fixed the cookie issue for reals now.

---
+++ static/lib/networkDefaults.js
@@ -0,0 +1,15 @@
+
+function isValidInput(n) {
+    return parseFloat(n) >= 0 && parseFloat(n) <= 1 && n != "";
+}
+
+function runSimulation(frm) {
+    diversity = frm.diversityP.value;
+    uniqueness = frm.uniquenessP.value;
+    reshare = frm.reshareP.value;
+    if (isValidInput(diversity) && isValidInput(uniqueness) && isValidInput(reshare)) {
+        simulating = true;
+    } else {
+        alert("Oops- make sure all fields are filled out! Accepted values are between 0.0 and 1.0.");
+    }
+}
